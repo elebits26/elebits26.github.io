03947b6a23ebd51fb368fb6a1319cf49d133d1ef
elebits26 <122620934+elebits26@users.noreply.github.com>
2024-02-05 02:08:03 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+//======================== header slider ========================
+
+
+$(".header__slider")
+  .slick({
+    autoplay: true,
+    fade: true,
+    arrows: false,
+    speed: 2000,
+    autoplaySpeed: 4000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  })
+
+
+//======================== AOS ========================
+
+
+AOS.init();
