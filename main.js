d7c77451d665c01056d9df8beb029ac6726db02d
elebits26 <122620934+elebits26@users.noreply.github.com>
2024-02-07 14:44:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,3 @@ $(".header__slider")
     pauseOnFocus: false,
     pauseOnHover: false,
   })
-
-  $('.header__slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-    $('.header__slider').slick('slickPlay');
-  });
